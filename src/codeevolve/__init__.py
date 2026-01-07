678929d1e21e6a3ce59c70655030a9e9acf51217
HenriqueAssumpcao <henriquesas2010@gmail.com>
2026-01-07 18:17:31 +0000

    update version

--- src/codeevolve/__init__.py
+++ src/codeevolve/__init__.py
@@ -9,4 +9,4 @@
 # This file initializes the main module for CodeEvolve.
 #
 # ===--------------------------------------------------------------------------------------===#
-__version__ = "2.0.0"
+__version__ = "0.2.0"
